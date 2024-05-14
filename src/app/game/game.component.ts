5bdea73f94747acaf23e1435202635816bb7be34
fshippman <fshippman@yahoo.com>
2024-05-14 13:43:02 +0200

    stock for played cards

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -9,31 +9,37 @@ import { Game } from './../../models/game';
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss',
 })
+
 export class GameComponent {
+
+
   pickCardAnimation = false;
-  currentCard: string | undefined = '';
-  game: Game | undefined; //Variable vom Typ game
+  currentCard?: string = '';
+  game?: Game; //Variable vom Typ game
+
 
   ngOnInit(): void {
     this.newGame();
   }
+
 
   newGame() {
     this.game = new Game();
     console.log(this.game);
   }
 
-  
-  takeCard() {
-    if (!this.pickCardAnimation && this.game !== undefined) {
-      this.currentCard = this.game.stack.pop(); //nimmt letzten Wert aus Array, gibt Wert zurück, gleichzeitig wird dieser aus Array entfernt
-      console.log(this.currentCard)
-      this.pickCardAnimation = true;
 
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      
+      this.currentCard = this.game?.stack.pop(); //nimmt letzten Wert aus Array, gibt Wert zurück, gleichzeitig wird dieser aus Array entfernt
+      this.pickCardAnimation = true; 
+
+      //after card animation finished (1000ms), push currentCard to playedCards
       setTimeout(() => {
         this.pickCardAnimation = false;
-      }, 1500);
-
+        this.game?.playedCards.push(this.currentCard!);
+      }, 1000);
     }
   }
 
