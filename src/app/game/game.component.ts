b886dc70c434e9548006356389cc8e14320fac3f
fshippman <fshippman@yahoo.com>
2024-05-14 09:31:15 +0200

    added game model

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,18 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Game } from './../../models/game';
 
 @Component({
   selector: 'app-game',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrl: './game.component.scss',
 })
 export class GameComponent {
   pickCardAnimation = false;
+  game!: Game; //Variable vom Typ game
 
-  takeCard(){
-    this.pickCardAnimation = true;
+  ngOnInit(): void {
+    this.newGame();
   }
 
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  takeCard() {
+    this.pickCardAnimation = true;
+  }
 }
