6f506b7011d8e4aaf308f892f49692b190cd0909
fshippman <fshippman@yahoo.com>
2024-05-17 11:21:02 +0200

    game info component

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -5,8 +5,8 @@ import { PlayerComponent } from '../player/player.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialog } from '@angular/material/dialog';
-
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+import { GameInfoComponent } from '../game-info/game-info.component';
 
 @Component({
   selector: 'app-game',
@@ -16,6 +16,7 @@ import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player
     PlayerComponent,
     MatButtonModule,
     MatIconModule,
+    GameInfoComponent
 
   ],
   templateUrl: './game.component.html',
