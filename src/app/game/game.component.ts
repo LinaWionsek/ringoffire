31fe6011bbcaed580a9f2918cd3bb67ff836b556
fshippman <fshippman@yahoo.com>
2024-06-10 15:24:47 +0200

    game.ts js doc

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,4 @@
-import { COMPILER_OPTIONS, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Game } from './../../models/game';
 import { PlayerComponent } from '../player/player.component';
@@ -11,9 +11,7 @@ import { GameInfoComponent } from '../game-info/game-info.component';
 import { SaveGameService } from '../firebase-services/save-game/save-game.service';
 import { DocumentData, addDoc } from '@angular/fire/firestore';
 import { ActivatedRoute } from '@angular/router';
-import { GameInterface } from '../interfaces/game.interface';
 import { Router } from '@angular/router';
-
 import { inject } from '@angular/core';
 import { Firestore, onSnapshot } from '@angular/fire/firestore';
 
@@ -49,38 +47,14 @@ export class GameComponent {
   async ngOnInit(): Promise<void> {
     this.route.params.subscribe((params) => {
       this.gameId = params['gameId'];
-      console.log('GAMEID', this.gameId);
-
-      //-------------------------------------\
-      // const q = this.SaveGameService.getSingleDocRef('games', this.gameId);
-
-      // onSnapshot(q, (singleGame) => {
-      //   console.log(singleGame.data(), 'IDDDDDDDDDDDDDDDDDDDDDD');
-      //   let gamedata = singleGame.data();
-      //   if (this.game) {
-      //     this.game.players = gamedata!['players'];
-      //     this.game.stack = gamedata!['players'];
-      //     this.game.playedCards = gamedata!['playedCards'];
-      //     this.game.currentPlayer = gamedata!['currentPlayer'];
-      //     this.game.currentCard = gamedata!['currentCard'];
-      //   }
-      // });
-      //-------------------------------------\
     });
 
     this.newGame();
   }
 
   async newGame() {
-    //GlEICH
     this.game = new Game();
     this.SaveGameService.addGame(this.game?.toJson());
-    //ALT
-    // let gameUpdate = await this.SaveGameService.getGameById(this.gameId);
-    // this.setGameData(gameUpdate);
-
-    //NEU
-    //-------------------------------------\
     const q = this.SaveGameService.getSingleDocRef('games', this.gameId);
     onSnapshot(q, (singleGame) => {
       console.log(singleGame.data(), 'IDDDDDDDDDDDDDDDDDDDDDD');
@@ -89,7 +63,6 @@ export class GameComponent {
         this.setGameData(gamedata);
       }
     });
-    //-------------------------------------\
   }
 
   setGameData(gameUpdate: DocumentData) {
@@ -103,48 +76,44 @@ export class GameComponent {
 
   takeCard() {
     if (this.game!.players.length > 0) {
-
       if (!this.game!.pickCardAnimation) {
-        let poper = this.game?.stack.pop();
-        this.game!.currentCard = poper ? poper : ''; //nimmt letzten Wert aus Array, gibt Wert zurück, gleichzeitig wird dieser aus Array entfernt
-        this.game!.pickCardAnimation = true;
-        this.game!.currentPlayer++; // (currentPlayer + 1) % length;
-        this.game!.currentPlayer =
-          this.game!.currentPlayer % this.game!.players.length;
-        // modulo sorgt dafür das obwohl currentplayer immer hoch gezählt wird,
-        //das auf die anzahl der spieler gerechnet wird und wieder bei 0 angefnagen wird, wenn alle spieler dran waren
-        //after card animation finished (1000ms), push currentCard to playedCards
+        this.animateCard();
         this.saveGame();
-        // PICK CARD ANIMATION BOOL IST NICHT AUFM SERVER GESPEICHERT!
-        setTimeout(() => {
-          this.game!.pickCardAnimation = false;
-          this.game!.playedCards.push(this.game!.currentCard!);
-          this.saveGame();
-
-          if (this.game!.playedCards.length === 2) {
-            this.gameEnd = true;
-            setTimeout(() => {
-              this.router.navigate(['/']);
-            }, 3000);
-          }
-
-        }, 1000);
+        this.updatePlayedCardStack();
       }
-
     } else {
       alert('Please create a player first!');
     }
   }
 
-  openDialog(): void {
-    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
-    //neue Funktion wird aufgerufen mit der variable result (die dialog textfeld eingegeben wird)
-    dialogRef.afterClosed().subscribe((result: string) => {
-      if (result && result.length > 0) {
-        this.game!.players.push(result);
-        this.saveGame();
+  animateCard() {
+    // modulo sorgt dafür das obwohl currentplayer immer hoch gezählt wird,
+    //das auf die anzahl der spieler gerechnet wird und wieder bei 0 angefnagen wird, wenn alle spieler dran waren
+    //after card animation finished (1000ms), push currentCard to playedCards
+    let poper = this.game?.stack.pop();
+    this.game!.currentCard = poper ? poper : ''; //nimmt letzten Wert aus Array, gibt Wert zurück, gleichzeitig wird dieser aus Array entfernt
+    this.game!.pickCardAnimation = true;
+    this.game!.currentPlayer++; // (currentPlayer + 1) % length;
+    this.game!.currentPlayer =
+      this.game!.currentPlayer % this.game!.players.length;
+  }
+
+  updatePlayedCardStack() {
+    setTimeout(() => {
+      this.game!.pickCardAnimation = false;
+      this.game!.playedCards.push(this.game!.currentCard!);
+      this.saveGame();
+      if (this.game!.playedCards.length === 2) {
+        this.endGame();
       }
-    });
+    }, 1000);
+  }
+
+  endGame() {
+    this.gameEnd = true;
+    setTimeout(() => {
+      this.router.navigate(['/']);
+    }, 3000);
   }
 
   saveGame() {
@@ -157,6 +126,16 @@ export class GameComponent {
       pickCardAnimation: this.game!.pickCardAnimation,
     });
   }
-
+  
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+    //neue Funktion wird aufgerufen mit der variable result (die dialog textfeld eingegeben wird)
+    dialogRef.afterClosed().subscribe((result: string) => {
+      if (result && result.length > 0) {
+        this.game!.players.push(result);
+        this.saveGame();
+      }
+    });
+  }
   /* /////////////////////////////////////// */
 }
--- src/models/game.ts
+++ src/models/game.ts
@@ -1,50 +1,64 @@
 export class Game {
-    public players: string[] = [];
-    public stack: string[] = [];
-    public playedCards: string[] = [];
-    public currentPlayer: number = 0;
-    public currentCard: string = '';
-    public pickCardAnimation = false;
+  public players: string[] = [];
+  public stack: string[] = [];
+  public playedCards: string[] = [];
+  public currentPlayer: number = 0;
+  public currentCard: string = '';
+  public pickCardAnimation = false;
 
-    constructor() {
-        for(let i=1; i < 14; i++){
-            this.stack.push('ace_' + i)
-            this.stack.push('clubs_' + i)
-            this.stack.push('hearts_' + i)
-            this.stack.push('diamonds_' + i)
-        }
-        shuffle(this.stack);
+
+  /**
+   * Initializes the game by populating the stack with card names and shuffling the stack.
+   *
+   * @return {void} This function does not return anything.
+   */
+  constructor() {
+    for (let i = 1; i < 14; i++) {
+      this.stack.push('ace_' + i);
+      this.stack.push('clubs_' + i);
+      this.stack.push('hearts_' + i);
+      this.stack.push('diamonds_' + i);
     }
-
-    // function returns json 
-    // this json returns variables with right values (defined players from above)
-    public toJson() {
-        return {
-            players: this.players ,
-            stack: this.stack,
-            playedCards: this.playedCards,
-            currentPlayer: this.currentPlayer,
-            currentCard: this.currentCard,
-            pickCardAnimation: this.pickCardAnimation 
-        }
-    }
-}
-// public weil wir in anderen Dateien darauf zugreifen wollen
-
-
-
-function shuffle(array: string[]) {
-    let currentIndex = array.length;
-  
-    // While there remain elements to shuffle...
-    while (currentIndex != 0) {
-  
-      // Pick a remaining element...
-      let randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
+    shuffle(this.stack);
   }
+
+
+  /**
+   * Returns a JSON representation of the current object.
+   * It includes the values of the properties of the object.
+   *
+   * @return {Object} The JSON representation of the object.
+   */
+  public toJson() {  //public because we want to access it in other files
+    return {
+      players: this.players,
+      stack: this.stack,
+      playedCards: this.playedCards,
+      currentPlayer: this.currentPlayer,
+      currentCard: this.currentCard,
+      pickCardAnimation: this.pickCardAnimation,
+    };
+  }
+}
+
+
+/**
+ * Shuffles the elements of an array in place using the Fisher-Yates algorithm.
+ *
+ * @param {string[]} array - The array to be shuffled.
+ * @return {void} This function does not return anything.
+ */
+function shuffle(array: string[]) {
+  let currentIndex = array.length;
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+}
